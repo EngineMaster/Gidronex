326f72cbdcbb6a286b57eb901a7eec9ca3b7bc5f
EngineMaster <gidronexservice@gmail.com>
2021-03-01 02:14:00 +0500

    stoark

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,7 +1,7 @@
 @include('includes.head')
 @include('includes.header')
 
-<section class="section_category">
+<section class="article">
         <h1>{{$post->title}}</h1>
     <div class="article_container">
         <img src="{{$post->subtitle}}" alt="" style="margin: 0 auto; display: flex;align-self: center;width: 400px" >
