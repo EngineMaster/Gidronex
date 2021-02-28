b1e8964907eaa8945dc2da8bbe7f10c4283b1fd5
EngineMaster <gidronexservice@gmail.com>
2021-03-01 02:16:18 +0500

    changes

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -4,7 +4,7 @@
 <section class="article">
         <h1>{{$post->title}}</h1>
     <div class="article_container">
-        <img src="{{$post->subtitle}}" alt="" style="margin: 0 auto; display: flex;align-self: center;width: 400px" >
+        <img src="{{$post->subtitle}}" alt="" style="margin: 0 auto; display: flex;align-self: center;width: 50%" >
         <div class="article_container_text">
             <p class="article1">{{$post->article}}</p>
             <p class="article2">{{$post->article2}}</p>
