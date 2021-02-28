90152fad0fc1f8fc96ee17cf3c394c38175edd6f
EngineMaster <gidronexservice@gmail.com>
2021-03-01 02:25:13 +0500

    shoulILive

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -7,6 +7,7 @@
         <img src="{{$post->subtitle}}" alt="article_image" style="margin: 0 auto; display: flex;align-self: center;width: 90%;border: 10px solid #fcba03;" >
         <div class="article_container_text">
             <p class="article1">{{$post->article}}</p>
+            <br><br>
             <p class="article2">{{$post->article2}}</p>
         </div>
     </div>
