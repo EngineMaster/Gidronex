7d39fdda2d96941dcb4ca14506d17fced313fb66
EngineMaster <gidronexservice@gmail.com>
2021-03-01 01:28:21 +0500

    posts

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -4,9 +4,8 @@
 <section class="section_category">
         <h1>{{$post->title}}</h1>
     <div class="article_container">
-        <img src="https://i.ytimg.com/vi/gsCXBiPWmno/maxresdefault.jpg" alt="" style="margin: 0 auto; display: flex;align-self: center;width: 400px" >
+        <img src="{{$post->subtitle}}" alt="" style="margin: 0 auto; display: flex;align-self: center;width: 400px" >
         <div class="article_container_text">
-            <p class="subtitle">{{$post->subtitle}}</p>
             <p class="article1">{{$post->article}}</p>
             <p class="article2">{{$post->article2}}</p>
         </div>
