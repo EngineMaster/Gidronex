b88c87e6d0d60a94736abc4bd021679ecc59f393
EngineMaster <gidronexservice@gmail.com>
2021-03-01 17:12:59 +0500

    top

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -20,27 +20,6 @@
             {{session()->get('success_message')}}
             @endif
     </section>
-
-
-<script>
-    window.onscroll = function() {myFunction2()};
-
-    //
-    //  the header
-    var header = document.querySelector('.navbar');
-
-    // Get the offset position of the navbar
-    var sticky = header.offsetTop;
-
-    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction2() {
-        if (window.pageYOffset > sticky) {
-            header.classList.add('sticky');
-        } else {
-            header.classList.remove('sticky');
-        }
-    }
-</script>
 </div>
 
 <script>
