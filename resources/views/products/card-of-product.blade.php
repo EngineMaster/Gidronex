d03d51aec13d7d3ce847e1abd283cc690ddac601
EngineMaster <gidronexservice@gmail.com>
2021-03-08 19:23:53 +0500

    fqwofj

--- resources/views/products/card-of-product.blade.php
+++ resources/views/products/card-of-product.blade.php
@@ -4,11 +4,12 @@
     <div class="product_layer">
         <div class="upper_menu">
             <h2>{{$prod->name}}</h2>
+            <p>{{$prod->description}}</p>
             <br>
         </div>
         <div class="lower_menu">
             <div class="category_name">{{$prod->name}}</div>
-            <div class="category_name">{{$prod->description}}</div>
+            <div class="category_name">{{$prod->index}}</div>
             <button class="btn_product_select"><a href="{{route('section',[$prod->id, $prod->name])}}">Подробнее</a></button>
         </div>
     </div>
