20772b469cb9dcd3be8016a0fa114274ca914124
EngineMaster <gidronexservice@gmail.com>
2021-03-10 01:39:55 +0500

    navbby

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,27 +1,65 @@
 <nav>
-    <a href="/" class="contacts_top"> <span class="material-icons call">call</span>+7 (922) 698-40-41</a>
-    <a href="/" class="contacts_top"><span class="material-icons mail">email</span>gidro_nex@mail.ru</a>
+    <div class="nav_wrapper">
+        <a href="/" class="contacts_top"> <span class="material-icons call">call</span>+7 (922) 698-40-41</a>
+        <div class="nav_wrapper_social_links">
+
+        </div>
+        <a href="/" class="contacts_top"><span class="material-icons mail">email</span>gidro_nex@mail.ru</a>
+    </div>
 </nav>
 
 <section class="navbar">
-        <div class="navbar_logo"><a href="/"> <img src="https://i.ibb.co/6P6GsQZ/Component-1.png" alt="company_logo" class="navbar_logo_icon_header"></a></div>
+
+        <div class="navbar_logo">
+            <a href="/">
+                <img src="https://i.ibb.co/6P6GsQZ/Component-1.png" alt="company_logo" class="navbar_logo_icon_header">
+            </a>
+        </div>
+
         <div class="navbar_wrapper">
             <ul class="navbar_wrapper_nav_links">
-                <li><a href="/" class="contact_menu">Главная</a></li>
-                <li><a href="/categories" class="contact_menu" id="contact_menu_hov">Ассортимент</a>
+                <li>
+                    <a href="/" class="contact_menu">
+                        Главная
+                    </a>
                 </li>
-                <li><a href="{{route('contact')}}" class="contact_menu">Связаться с нами</a></li>
-                <li><a href="/posts" class="contact_menu">FAQ</a></li>
-                <li><a href="{{route('basket')}}" class="contact_menu"><span class="material-icons search" style=" color: white; margin-bottom: 50px">search</span></a></li>
-                <li><a href="{{route('basket')}}" class="contact_menu"><span class="material-icons search" style="color: white">shopping_basket</span><span>{{\Cart::session($_COOKIE['sessionIds'])->getTotalQuantity()}}</span></a></li>
+                <li>
+                    <a href="/categories" class="contact_menu" id="contact_menu_hov">
+                        Ассортимент
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('contact')}}" class="contact_menu">
+                        Связаться с нами
+                    </a>
+                </li>
+                <li>
+                    <a href="/posts" class="contact_menu">
+                        FAQ
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('basket')}}" class="contact_menu">
+                        <span class="material-icons search" style=" color: white; margin-bottom: 50px">
+                            search
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('basket')}}" class="contact_menu">
+                        <span class="material-icons search" style="color: white">shopping_basket</span>
+                        <span>{{\Cart::session($_COOKIE['sessionIds'])->getTotalQuantity()}}</span>
+                    </a>
+                </li>
             </ul>
         </div>
 
-    <div class="burger">
-        <div class="line1"></div>
-        <div class="line2"></div>
-        <div class="line3"></div>
-    </div>
+        <div class="burger">
+            <div class="line1"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+        </div>
+
 </section>
 
 
