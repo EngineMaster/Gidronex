fffb341aa776ffc8c8e3f0c16d3e485942d40865
EngineMaster <gidronexservice@gmail.com>
2021-03-16 00:21:42 +0500

    skrt

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,53 +5,53 @@
                 <li><span class="footer_nav">Запчасти для экскаваторов:</span></li>
                 <li><a href="{{route('categories')}}">ЭО-хххх, ЭО-хххх</a></li>
                 <li> <a href="{{route('categories')}}">ЭЭО-хуху, ЭО-хуху, ЭО-хуху1</a></li>
-                <li> <a href="">ЭЭО-дддд, ЭО-дддд1 </a></li>
-                <li><a href="">ЭЭО-уууу, ЭО-ууууу</a></li>
-                <li><a href="">ЭЭО-цццц, ЭО-ц1ц1</a></li>
-                <li><a href="">ЭЭО-мммм, ЭО-мм11</a></li>
-                <li><a href="">ЭЕК-12, ЕК-14, ЕК-18</a></li>
-                <li><a href=""><span class="footer_nav">Запчасти для драглайнов:</span></a></li>
-                <li><a href="">ЭЭО-ххху</a></li>
-                <li><a href="">ЭЭО-хххх</a></li>
-                <li><a href="">ЭЭО-хххх</a></li>
+                <li> <a href="{{route('categories')}}">ЭЭО-дддд, ЭО-дддд1 </a></li>
+                <li><a href="{{route('categories')}}">ЭЭО-уууу, ЭО-ууууу</a></li>
+                <li><a href="{{route('categories')}}">ЭЭО-цццц, ЭО-ц1ц1</a></li>
+                <li><a href="{{route('categories')}}">ЭЭО-мммм, ЭО-мм11</a></li>
+                <li><a href="{{route('categories')}}">ЭЕК-12, ЕК-14, ЕК-18</a></li>
+                <li><a href="{{route('categories')}}"><span class="footer_nav">Запчасти для драглайнов:</span></a></li>
+                <li><a href="{{route('categories')}}">ЭЭО-ххху</a></li>
+                <li><a href="{{route('categories')}}">ЭЭО-хххх</a></li>
+                <li><a href="{{route('categories')}}">ЭЭО-хххх</a></li>
             </ul>
         </div>
 
         <div class="column_of_footer">
             <ul>
                 <li><span class="footer_nav">Фильтры:</span></li>
-                <li><a href="">Маслянные</a></li>
-                <li> <a href="">Воздушные</a></li>
-                <li> <a href="">Топливные </a></li>
-                <li><a href=""><span class="footer_nav">Гидроматоры</span></a></li>
-                <li><a href=""><span class="footer_nav">Гидроматоры</span></a></li>
-                <li><a href=""><span class="footer_nav">Гидроматоры</span></a></li>
-                <li><a href=""><span class="footer_nav">Гидроматоры</span></a></li>
-                <li><a href=""><span class="footer_nav">Запчасти для драглайнов:</span></a></li>
+                <li><a href="{{route('categories')}}">Маслянные</a></li>
+                <li> <a href="{{route('categories')}}">Воздушные</a></li>
+                <li> <a href="{{route('categories')}}">Топливные </a></li>
+                <li><a href="{{route('categories')}}"><span class="footer_nav">Гидроматоры</span></a></li>
+                <li><a href="{{route('categories')}}"><span class="footer_nav">Гидроматоры</span></a></li>
+                <li><a href="{{route('categories')}}"><span class="footer_nav">Гидроматоры</span></a></li>
+                <li><a href="{{route('categories')}}"><span class="footer_nav">Гидроматоры</span></a></li>
+                <li><a href="{{route('categories')}}"><span class="footer_nav">Запчасти для драглайнов:</span></a></li>
             </ul>
         </div>
         <div class="column_of_footer">
             <ul>
                 <li><span class="footer_nav">Гидроаппаратура:</span></li>
-                <li><a href="">Блоки управления</a></li>
-                <li> <a href="">Гидроклапана</a></li>
-                <li> <a href="">Гидрорасприделители</a></li>
-                <li><a href="">Гидрорули</a></li>
-                <li><a href="">Гидробуры</a></li>
-                <li><a href="">IMPULSE</a></li>
-                <li><a href="">DELTA</a></li>
-                <li><a href=""><span class="footer_nav">Гидромолоты</span></a></li>
-                <li><a href="">IMPULSE</a></li>
-                <li><a href="">DELTA</a></li>
+                <li><a href="{{route('categories')}}">Блоки управления</a></li>
+                <li> <a href="{{route('categories')}}">Гидроклапана</a></li>
+                <li> <a href="{{route('categories')}}">Гидрорасприделители</a></li>
+                <li><a href="{{route('categories')}}">Гидрорули</a></li>
+                <li><a href="{{route('categories')}}">Гидробуры</a></li>
+                <li><a href="{{route('categories')}}">IMPULSE</a></li>
+                <li><a href="{{route('categories')}}">DELTA</a></li>
+                <li><a href="{{route('categories')}}"><span class="footer_nav">Гидромолоты</span></a></li>
+                <li><a href="{{route('categories')}}">IMPULSE</a></li>
+                <li><a href="{{route('categories')}}">DELTA</a></li>
             </ul>
         </div>
         <div class="column_of_footer_about_us">
             <ul>
-                <li><a href="">О нас</a></li>
-                <li> <a href="">Оплата</a></li>
-                <li><a href="">Доставка</a></li>
-                <li><a href="">Акции</a></li>
-                <li><a href="">Отзывы
+                <li><a href="#nav">О нас</a></li>
+                <li> <a href="#nav">Оплата</a></li>
+                <li><a href="#nav">Доставка</a></li>
+                <li><a href="#nav">Акции</a></li>
+                <li><a href="#nav">Отзывы
                     </a></li>
             </ul>
         </div>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,4 @@
-<nav>
+<nav id="nav">
         <a href="/" class="contacts_top"> <span class="material-icons call">call</span>+7 (922) 698-40-41</a>
 
         <a href="/" class="contacts_top"><span class="material-icons mail">email</span>gidro_nex@mail.ru</a>
