42afe177e713d506f63d8a8df042321fa2003848
EngineMaster <gidronexservice@gmail.com>
2021-04-08 19:21:59 +0500

    metrika

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -3,6 +3,21 @@
 @section('content')
     @include('includes.header')
 <body onload=" myFunction()">
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(75386056, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/75386056" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 <div id="loader-42"></div>
 <div style="display:none;" id="myDiv" class="animate-bottom">
     <section class="categories_wrapper">
